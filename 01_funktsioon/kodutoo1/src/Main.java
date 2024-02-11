b9b940fa7d9cdb6162ddb7170be6c4ad7ca32d2c
Jan Markus Kähara <146342688+JanMarkusK@users.noreply.github.com>
2024-02-11 21:17:49 +0200

    Update Main.java

--- 01_funktsioon/kodutoo1/src/Main.java
+++ 01_funktsioon/kodutoo1/src/Main.java
@@ -1,26 +1,36 @@
-//Näide
 public class Main {
     public static void main(String[] args) {
         //Kodus teha 3 funktsiooni milles on kasutatud vähemalt nelja erinevat tüüpi (String, int, long, char, boolean jne)
         //Teha igast funktsioonist 2 erineva sisendiga väljakutset
         //Näited
-        double koguMaksumus1 = arvutaTaksoMaksuvus (tunnid; 3)
+        double koguMaksumus1 = arvutaTaksoMaksuvus (3);
         System.out.println(koguMaksumus1);
-        double koguMaksumus2 = arvutaTaksoMaksuvus (tunnid; 3)
+        double koguMaksumus2 = arvutaTaksoMaksuvus (4);
         System.out.println(koguMaksumus2);
         
-        String hinnang1 = TemperatuuriHinnang (-2, -3)
+        String hinnang1 = TemperatuuriHinnang (2.2);
         System.out.println(hinnang1);
-        String hinnang2 = TemperatuuriHinnang (-3, -5)
+        String hinnang2 = TemperatuuriHinnang (-2.1);
         System.out.println(hinnang2);
         
-        prindiTsykkel(tahtedeArv "Pikk sõna");
-        prindiTsykkel(tahtedeArv "Lyh");
+        prindiTsykkel("Pikk sõna");
+        prindiTsykkel("Lyh");
         
         //Kodutöö
-        sonaVordlus(sona "elu");
-        sonaVordlus(sona "muu sõna");
+        sonaLeidmine("elu");
+        sonaLeidmine("muu sõna");
+
+        int summa1 = viiegaLiitmine (1);
+        System.out.println(summa1);
+        int summa2 = viiegaLiitmine (3);
+        System.out.println(summa2);
+
+        double kartuliMaksumus1 = kartuliHind(8);
+        System.out.println(kartuliMaksumus1);
+        double kartuliMaksumus2 = kartuliHind(11);
+        System.out.println(kartuliMaksumus2);
     }
+    //Näited
     private static double arvutaTaksoMaksuvus(int tunnid) {
         return 3+0.9*tunnid;
     }
@@ -37,12 +47,18 @@ public class Main {
         }
     }
     //Kodutöö
-    private static void sonaVordlus(String sona) {
-        if (sona = "elu") {
+    private static void sonaLeidmine(String sona) {
+        if (sona.equals("elu")) {
             System.out.println("Leidsid õige sõna");
         } else {
             System.out.println("Ei vedanud");
         }
+    }
+    public static int viiegaLiitmine(int arv) {
+        return 5+arv;
+    }
+    public static double kartuliHind (int kartuliteArv) {
+        return 0.56 * kartuliteArv;
     }
 }
 
