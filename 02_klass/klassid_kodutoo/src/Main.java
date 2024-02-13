5ffa5c36a8ec0e7530e1be910fb1fd5d8b274363
JanMarkusK <jmktrk@tlu.ee>
2024-02-13 15:29:02 +0200

    veel

--- 02_klass/klassid_kodutoo/src/Main.java
+++ 02_klass/klassid_kodutoo/src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {System.out.println("Hello world!");
         //IDEE KODU TÖÖKS: Relva salves kuulide arvutamine ja siis vaatad kas paned salve relva sisse
-
+        User user = new User("m", true, 18,'x',599);
+        User user = new User("m", true, 18,'x',599);
     }
 }
---
+++ 02_klass/klassid_kodutoo/src/User.java
@@ -0,0 +1,22 @@
+public class user {
+    String nimi;
+    boolean aktiivne;
+    int vanus;
+    char lyhend;
+    double raha;
+
+    public user(String nimi, boolean aktiivne, int vanus, char lyhend, double raha) {
+        this.nimi = nimi;
+        this.aktiivne = aktiivne;
+        this.vanus = vanus;
+        this.lyhend = lyhend;
+        this.raha = raha;
+    }
+    public void muudaAktiivne(){
+        aktiivne = !aktiivne;
+    }
+    public double lisaRaha(double summa){
+        raha = raha + summa;
+        return raha;
+    }
+}
