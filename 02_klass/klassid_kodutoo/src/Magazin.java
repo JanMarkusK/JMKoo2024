b836b65939fcf04a054ed810cdb52127f689871b
VioletSAAB <janmarkuskahara@gmail.com>
2024-02-17 22:18:21 +0200

    Täna kirjutatud kood

---
+++ 02_klass/klassid_kodutoo/src/Magazin.java
@@ -0,0 +1,27 @@
+public class Magazin {
+    int magazinCapacity;
+    int magazinCount;
+    int bullets;
+
+    public Gun(int magazinCapacity, int magazinCount, int bullets) {
+        this.magazinCapacity = magazinCapacity;
+        this.magazinCount = magazinCount;
+        this.bullets = bullets;
+    }
+    public void magazinReloading(int magazinCount) {
+        while (magazinCount < magazinCapacity) {
+            magazinCount++;
+            bullets--;
+        }
+        if (magazinCount == magazinCapacity) {
+            System.out.println("Salv on laetud");
+        }
+        if (bullets == 0) {
+            System.out.println("I need more BULLETS!");
+        }
+    }
+    public int bulletSupply(int bulletsBrought) {
+        bullets = bullets + bulletsBrought;
+        return bullets;
+    }
+}
--- 02_klass/klassid_kodutoo/src/Main.java
+++ 02_klass/klassid_kodutoo/src/Main.java
@@ -1,7 +1,10 @@
 public class Main {
     public static void main(String[] args) {System.out.println("Hello world!");
         //IDEE KODU TÖÖKS: Relva salves kuulide arvutamine ja siis vaatad kas paned salve relva sisse
-        User user = new User("m", true, 18,'x',599);
-        User user = new User("m", true, 18,'x',599);
+        Gun Gun = new Magazin(30, 0,400);
+        Gun Gun2 = new Magazin(30, 0,400);
+
+        magazin.magazinReloading(0);
+        magazin.bulletSupply(200);
     }
 }
